43472a237d94d4c98a7e0e4dacae48ff00ba03d5
rajitchadar59 <rajitathiya123@gmail.com>
2026-09-03 19:15:10 +0530

    change backend url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
-const IS_PROD = false; // Toggle to true before Render deployment
+const IS_PROD = true; 
 
-const server = IS_PROD ? "https://task-project-tracking.com/api" : "http://localhost:5000/api";
+const server = IS_PROD ? "https://project-task-tracker-backend-36mn.onrender.com" : "http://localhost:5000/api";
 
 export default server;
